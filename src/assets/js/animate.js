34ec15ff39679f877985ec10c22b1f15af7c5d46
João Neto <joaolima882018@gmail.com>
2023-07-02 21:42:06 +0100

    Animação

---
+++ src/assets/js/animate.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("form");
+const boxForm = document.getElementById("box__form");
+const animateShakeX = "animate__shakeX";
+
+form.addEventListener("submit", handleSubmit);
+boxForm.addEventListener("animationend", handleAnimation);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const inputsContact = form.querySelectorAll("[type=text], [type=email]");
+
+    for (const input of Array.from(inputsContact)) {
+        const label = input.closest("label");
+        if (!validateContact(input)) {
+            showStatus(label);
+        }else{
+            showStatus(label, false);
+        }
+    }
+
+
+    boxForm.classList.add(animateShakeX);
+}
+
+function handleAnimation(evt) {
+    if (evt.animationName === "shakeX") {
+        boxForm.classList.remove(animateShakeX);
+    }
+}
+
+function validateContact(input) {
+    const isValidEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)+$/.test(input.value);
+    const isValidTel = /^(\+244)?9[123459]\d{7}$/.test(input.value);
+
+    return  isValidEmail || isValidTel;
+}
+
+function showStatus(label, error = true) {
+    const status = label.querySelectorAll(".status");
+    status.forEach(s=>s.style.display = "");
+
+    if (error) {
+        status[0].style.display = "inline";
+    } else {
+        status[1].style.display = "inline";
+    }
+}
